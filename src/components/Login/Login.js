6d26446a6ba05ae490e50a6f19750898f8af33f3
meganmfisher <meganmichellefisher@gmail.com>
2018-05-02 20:39:47 -0600

    redux up

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,7 +6,7 @@ export default class Login extends Component {
     render() {
         return (
             <div> 
-                <h1>Login</h1> 
+                <a href={ process.env.REACT_APP_LOGIN }><button>Login</button></a>
             </div> 
         )
     }
