d4650a78649a01c912da4d10f8dc075ef67da54e
meganmfisher <meganmichellefisher@gmail.com>
2018-05-06 12:39:58 -0600

    material ui in

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,29 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './Footer.css';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class Footer extends Component {
-    constructor() {
-        super() 
-        this.state = {
-            firstName: '',
-            lastName: '', 
-            email: '', 
-            description: ''
-        }
-
-        this.handleInput = this.handleInput.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
-
-    handleInput(e, label) {
-        this.setState({
-            [label]: e.target.value
-        })
-    }
-
     handleSubmit() {
-        console.log(this.firstName.value)
         if(!this.firstName.value || !this.lastName.value || !this.email.value || !this.description.value) {
             alert('Please complete all boxes')
         } else {
@@ -33,7 +14,6 @@ export default class Footer extends Component {
                 email: this.email.value,
                 description: this.description.value
             }
-            console.log('body', body)
             axios.post('/api/contactAdmin', body).then(res => {
                 this.firstName.value = '';
                 this.lastName.value = '';
@@ -50,7 +30,7 @@ export default class Footer extends Component {
                 <input placeholder='Last Name' ref={input => this.lastName = input}/>
                 <input placeholder='Email' ref={input => this.email = input}/>
                 <input placeholder='Message' ref={input => this.description = input}/>
-                <button onClick={this.handleSubmit}>Submit</button>
+                <RaisedButton label='Submit' onClick={this.handleSubmit.bind(this)} />
             </div> 
         )
     }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import registerServiceWorker from './registerServiceWorker';
 import { HashRouter } from 'react-router-dom';
 
@@ -10,9 +11,11 @@ import store from './store';
 
 ReactDOM.render(
     <Provider store={store}>
-        <HashRouter>
-            <App />
-        </HashRouter>
+        <MuiThemeProvider>
+            <HashRouter>
+                <App />
+            </HashRouter>
+        </MuiThemeProvider>
     </Provider>, document.getElementById('root'));
 
 registerServiceWorker();
